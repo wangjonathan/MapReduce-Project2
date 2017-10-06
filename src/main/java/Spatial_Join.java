8a07d5d9f04625e8054eff8d2e06057f4d83442e
JonathanWang <wangjonathan1992@gmail.com>
2017-10-06 14:33:04 -0400

    Add user input

--- src/main/java/Spatial_Join.java
+++ src/main/java/Spatial_Join.java
@@ -38,23 +38,29 @@ public class Spatial_Join {
 
         private void loadUserInMemory(Mapper<Object, Text, Text, Text>.Context context) {
             // user.log is in distributed cache
-            try (BufferedReader br = new BufferedReader(new FileReader("./input/Dataset_R.txt"))) {
-                String line;
-                while ((line = br.readLine()) != null) {
+            String input = context.getConfiguration().get("input");
+            if(input != null) {
+                coordinate.put("user", input.substring(2, input.length() - 1));
+            } else {
+                try (BufferedReader br = new BufferedReader(new FileReader("./input/Dataset_R.txt"))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
 //                    System.out.println(line.toString());
 //                    System.out.println(line);
-                    String[] str = line.substring(1, line.length() - 1).split(",");
-                    StringBuilder tmp = new StringBuilder();
-                    tmp.append(str[1] + ',' + str[2] + ',' + str[3] + ',' + str[4]);
-                    coordinate.put(str[0], tmp.toString());
+                        String[] str = line.substring(1, line.length() - 1).split(",");
+                        StringBuilder tmp = new StringBuilder();
+                        tmp.append(str[1] + ',' + str[2] + ',' + str[3] + ',' + str[4]);
+                        coordinate.put(str[0], tmp.toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+
         }
     }
 
-    public static class SpatialJoinReducer extends Reducer<Text, Text, Text, Text>{
+    public static class SpatialJoinReducer extends Reducer<Text, Text, Text, NullWritable>{
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
             count++;
             for(Text text : values) {
@@ -62,26 +68,26 @@ public class Spatial_Join {
                 int y = Integer.valueOf(text.toString());
 //                System.out.println(x + "/" + y);
 //                System.out.println("------------");
-                isContained(context, x, y, key);
+                isContained(context, x, y);
 //                if(tmp.length() != 0) {
 //                    context.write(new Text(tmp), NullWritable.get());
 //                }
             }
         }
-        private static String isContained(Context context, int x_coordinate, int y_coordinate, Text key) throws IOException, InterruptedException{
+        private static String isContained(Context context, int x_coordinate, int y_coordinate) throws IOException, InterruptedException{
             for(Map.Entry<String, String> entry : coordinate.entrySet()) {
                 String[] str = entry.getValue().split(",");
                 int x_left = Integer.valueOf(str[0]);
                 int x_right = Integer.valueOf(str[2]);
-                int y_left = Integer.valueOf(str[1]);
-                int y_right = Integer.valueOf(str[3]);
+                int y_low = Integer.valueOf(str[1]);
+                int y_high = Integer.valueOf(str[3]);
 
-                if(x_coordinate >= x_left && x_coordinate <= x_right && y_coordinate >= y_left && y_coordinate <= y_right) {
+                if(x_coordinate >= x_left && x_coordinate <= x_right && y_coordinate >= y_low && y_coordinate <= y_high) {
 //                    System.out.println(entry.getKey() + "/" + x_left + "/" + x_right + "/" + y_left + "/" + y_right);
 //                    System.out.println(x_coordinate + "/" + y_coordinate);
                     StringBuilder res = new StringBuilder();
                     res.append("<" + entry.getKey() + ',' + '(' + x_coordinate + ',' + y_coordinate + ')' + '>');
-                    context.write(key, new Text(res.toString()));
+                    context.write(new Text(res.toString()), NullWritable.get());
                     return res.toString();
                 } else return "";
             }
@@ -93,14 +99,16 @@ public class Spatial_Join {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("input", "W(1000,1500,3000,9000)");
 //        Path p1=new Path(args[0]);
 //        Path p2=new Path(args[1]);
 //        Path p3=new Path(args[2]);
-        if (args.length != 2) {
-            System.err.println("Usage: taska <HDFS input file> <HDFS output file>");
-            System.exit(2);
-        }
+//        if (args.length != 2) {
+//            System.err.println("Usage: taska <HDFS input file> <HDFS output file>");
+//            System.exit(2);
+//        }
         Job job = new Job(conf, "TaskA");
+//        job.set("inputString", "data");
         job.setJarByClass(Spatial_Join.class);
         job.setMapperClass(SpatialJoinMapper.class);
         job.setMapOutputKeyClass(Text.class);
@@ -110,7 +118,7 @@ public class Spatial_Join {
 
         job.setOutputKeyClass(Text.class);
         job.setNumReduceTasks(2);
-        job.setOutputValueClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
 
 //        MultipleInputs.addInputPath(job, p1, TextInputFormat.class, SpatialJoinMapper.class);
 //        MultipleInputs.addInputPath(job,p2, TextInputFormat.class, SpatialJoin2Mapper.class);
